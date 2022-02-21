7e7551e561a643b36f0dfcb8fe862c73fe8cae6e
Adele <adele.therias@live.com>
2022-02-21 20:53:15 +0100

    update gmap.h

--- hw/01/cpp/src/Gmap.h
+++ hw/01/cpp/src/Gmap.h
@@ -1,5 +1,5 @@
 #pragma once
-
+//ioanna
 #include "Point.h"
 
 struct Point;
@@ -19,6 +19,7 @@ One way to do this is by using pointers. eg. define a member on the dart struct 
 
   Struct Dart {
     // involutions:
+    // indicate that a0 does not point to an object
     Dart* a0 = nullptr;
     // ...
 
@@ -37,14 +38,23 @@ Then you could create and link Darts like:
 
 struct Dart {
   // involutions:
-  // ..
+  int a0;
+  int a1;
+  int a2;
+  int a3;
 
-  // cells:
-  // ...
+  // cells - ids that link to other tables?:
+  Vertex cell_0;
+  Edge cell_1;
+  Face cell_2;
+  Volume cell_3;
 
 };
 
 struct Vertex {
+    // vertex id
+    char vertex_id;
+
   // the coordinates of this vertex:
   Point point;
 
@@ -62,24 +72,54 @@ struct Vertex {
 };
 
 struct Edge {
-  // a dart incident to this Edge:
+//    //edge id - maybe just use index
+//    char edge_id;
+
+    // start and end point
+    Vertex *edge_start;
+    Vertex *edge_end;
+
+    // a dart incident to this Edge:
   // ...
 
   // function to compute the barycenter for this Edge (needed for triangulation output):
-  // Point barycenter() {}
+  Point barycenter(Point edge_end, Point edge_start) {
+      (edge_end - edge_start)/2;
+  }
 };
 
 struct Face {
-  // a dart incident to this Face:
+    // unique face id - use index
+//    char face_id;
+
+    // vector of face vertices
+    std::vector<Vertex> face_vertices;
+
+// a dart incident to this Face:
+  // ...
   // ...
 
   // function to compute the barycenter for this Face (needed for triangulation output):
-  // Point barycenter() {}
+   Point barycenter(std::vector<Point> face_vertices) {
+      float number_vertices = face_vertices.size()
+      for (i = 0; i <= number_vertices; number_vertices ++){
+          sum_vertices += p;
+      }
+      return sum_vertices/number_vertices;
+   }
 
 };
 
 struct Volume {
-  // a dart incident to this Volume:
+    // unique volume id
+    char vol_id;
+
+    // vector of volume face vertices
+    std::vector Point volume_vertices;
+
+    // barycenter?
+
+    // a dart incident to this Volume:
   // ...
 
 };
