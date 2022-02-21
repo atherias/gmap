8490cdf80ff77ab79b949b96356109e50cfcafbc
Hugo Ledoux <h.ledoux@tudelft.nl>
2022-02-21 16:39:48 +0100

    Improve the cpp help code

--- hw/02/cpp/src/main.cpp
+++ hw/02/cpp/src/main.cpp
@@ -4,8 +4,11 @@
 #include <string>
 #include "json.hpp"
 
-
 using json = nlohmann::json;
+
+int   get_no_roof_surfaces(json &j);
+void  list_all_vertices(json& j);
+
 
 
 int main(int argc, const char * argv[]) {
@@ -15,6 +18,12 @@ int main(int argc, const char * argv[]) {
   json j;
   input >> j;
   input.close();
+
+  //-- get total number of RoofSurface in the file
+  int noroofsurfaces = get_no_roof_surfaces(j);
+  std::cout << "Total RoofSurface: " << noroofsurfaces << std::endl;
+
+  // list_all_vertices(j);
 
   //-- print out the number of Buildings in the file
   int nobuildings = 0;
@@ -40,8 +49,49 @@ int main(int argc, const char * argv[]) {
   o << j.dump(2) << std::endl;
   o.close();
 
-
   return 0;
 }
 
 
+int get_no_roof_surfaces(json &j) {
+  int total = 0;
+  for (auto& co : j["CityObjects"].items()) {
+    for (auto& g : co.value()["geometry"]) {
+      if (g["type"] == "Solid") {
+        for (auto& shell : g["semantics"]["values"]) {
+          for (auto& s : shell) {
+            if (g["semantics"]["surfaces"][s.get<int>()]["type"].get<std::string>().compare("RoofSurface") == 0) {
+              total += 1;
+            }
+          }
+        }
+      }
+    }
+  }
+  return total;
+}
+
+
+void list_all_vertices(json& j) {
+  for (auto& co : j["CityObjects"].items()) {
+    std::cout << "= CityObject: " << co.key() << std::endl;
+    for (auto& g : co.value()["geometry"]) {
+      if (g["type"] == "Solid") {
+        for (auto& shell : g["boundaries"]) {
+          for (auto& surface : shell) {
+            for (auto& ring : surface) {
+              std::cout << "---" << std::endl;
+              for (auto& v : ring) { 
+                std::vector<int> vi = j["vertices"][v.get<int>()];
+                double x = (vi[0] * j["transform"]["scale"][0].get<double>()) + j["transform"]["translate"][0].get<double>();
+                double y = (vi[1] * j["transform"]["scale"][1].get<double>()) + j["transform"]["translate"][1].get<double>();
+                double z = (vi[2] * j["transform"]["scale"][2].get<double>()) + j["transform"]["translate"][2].get<double>();
+                std::cout << std::setprecision(2) << std::fixed << v << " (" << x << ", " << y << ", " << z << ")" << std::endl;                
+              }
+            }
+          }
+        }
+      }
+    }
+  }
+}
