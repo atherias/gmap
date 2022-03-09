1a8fad90092f4f509ac23409dac6e4c765b733db
Adele <adele.therias@live.com>
2022-03-09 12:46:22 +0100

    gmap only

---
+++ 01/cpp/cmake-build-debug/CMakeFiles/hw01.dir/DependInfo.cmake
@@ -0,0 +1,28 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/mnt/c/Users/Adele/Documents/TU-Delft/Q3/GEO1004_3D-Modelling/geo1004.2022/hw/01/cpp/src/main.cpp" "/mnt/c/Users/Adele/Documents/TU-Delft/Q3/GEO1004_3D-Modelling/geo1004.2022/hw/01/cpp/cmake-build-debug/CMakeFiles/hw01.dir/src/main.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ 01/cpp/src/Point.h
@@ -0,0 +1,87 @@
+#pragma once
+
+/*
+Structure to represent a 3D point. Various operations are defined. eg creation 
+using `Point p(x,y,z)`, accessing a coordinate as `p[0]` (equivalent to `p.x`), vector addition/substraction 
+as `p+q` or `p-q`, multiplication/division with a scalar as `p*s` or `p/s`, dot/cross 
+product as `p.dot(q)` or `p.cross(q)`.
+*/
+struct Point {
+  float x, y, z;
+  
+  // constructor without arguments; initialises all coordinates on 0.
+  Point() : x(0), y(0), z(0)
+  {}
+  
+  // constructor with x,y,z arguments;
+  Point(const float &x, const float &y, const float &z) : x(x), y(y), z(z)
+  {}
+  
+  // non-const coordinate accessor with `[]`
+  float &operator[](const int &coordinate) {
+    if (coordinate == 0) return x;
+    else if (coordinate == 1) return y;
+    else if (coordinate == 2) return z;
+  }
+  
+  // const coordinate accessor with `[]`
+  float operator[](const int &coordinate) const {
+    if (coordinate == 0) return x;
+    else if (coordinate == 1) return y;
+    else if (coordinate == 2) return z;
+  }
+
+  // assignment operator. Assigns values from other point to this point
+  void operator=(const Point &other) {
+    x=other.x;
+    y=other.y;
+    z=other.z;
+  }
+  
+  // addition operator (yields a new point)
+  const Point operator+(const Point &other) const {
+    return Point(x+other.x, y+other.y, z+other.z);
+  }
+  
+  // assignment/addition operator. Adds other point to this point.
+  void operator+=(const Point &other) {
+    x+=other.x;
+    y+=other.y;
+    z+=other.z;
+  }
+  
+  // subtraction operator (yields a new point)
+  const Point operator-(const Point &other) const {
+    return Point(x-other.x, y-other.y, z-other.z);
+  }
+  
+  // element-wise multiplication operator (yields a new point)
+  const Point operator*(const float &other) const {
+    return Point(x*other, y*other, z*other);
+  }
+  
+  // division operator (yields a new point)
+  const Point operator/(const float &other) const {
+    return Point(x/other, y/other, z/other);
+  }
+  
+  // dot product operator
+  float dot(const Point &other) const {
+    return x*other.x + y*other.y + z*other.z;
+  }
+  
+  // cross product operator
+  const Point cross(const Point &other) const {
+    return Point(y*other.z-z*other.y, -(x*other.z-z*other.x), x*other.y-y*other.x);
+  }
+
+  bool operator==(const Point &other) {
+      return (x==other.x && y==other.y && z==other.z);
+  }
+};
+
+// function to print the coordinates of a point using eg `std::cout << p`, where p is a Point struct
+std::ostream& operator<<(std::ostream& os, const Point p) {
+  os << "(" << p.x << ", " << p.y << ", " << p.z << ")";
+  return os;
+}
